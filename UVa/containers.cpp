74b657808150d66dc47e60d181c05041fc0eb3e7
Arnav Kumar <arnavcalgary12@gmail.com>
2020-06-22 16:22:39 -0600

    finished containers

--- UVa/containers.cpp
+++ UVa/containers.cpp
@@ -5,13 +5,17 @@
 using namespace std;
 
 int findIndex(vector<char> stacks, char container) {
+    if (stacks[0] >= container) {
+        return 0;
+    }
+
     int min = 1;
     int max = stacks.size();
     int i;
     bool bigger;
     bool smaller; 
 
-    while (i = (min + max) / 2, smaller = !(stacks[i-1] < container), bigger = (stacks[i] < container), !(smaller || bigger)) {
+    while (i = (min + max) / 2, smaller = (stacks[i-1] >= container), bigger = (stacks[i] < container), (smaller || bigger)) {
         if (smaller) {
             max = i;
         } if (bigger) {
@@ -23,8 +27,30 @@ int findIndex(vector<char> stacks, char container) {
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    string line;
+    vector<char> stackTops;
+    int caseNum = 0;
 
+    while (getline(cin, line), line != "end") {
+        stackTops.clear();
+        stackTops.resize(0);
+
+        while (!line.empty()) {
+            if (stackTops.empty() || stackTops[stackTops.size() - 1] < line.front()) {
+                stackTops.push_back(line.front());
+            } else {
+                stackTops[findIndex(stackTops, line.front())] = line.front();
+            }
+            line.erase(0, 1);
+        }
+
+        caseNum++;
+
+        cout << "Case " << caseNum << ": " << stackTops.size() << endl; 
+    }
 
     return 0;
 }
