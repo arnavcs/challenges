16101047c1dfa4ff89b69bcf50ecdae96d9838c8
Arnav Kumar <arnavcalgary12@gmail.com>
2020-12-23 11:18:57 -0700

    solved CCC 2010 S2

--- CCC/Senior 2010/huffmanEncoding.cpp
+++ CCC/Senior 2010/huffmanEncoding.cpp
@@ -2,37 +2,66 @@
 // Huffman Encoding CCC 2010 senior
 
 #include <bits/stdc++.h>
-// #include "../debug.h"
 using namespace std;
 
-// general macros
-#define DEBUG(x) cout << "> " << #x << ": " << x << endl;
-#define fall(i, a, b) for (int i = a; i <= b; ++i)
 
-// bit macros
-#define leastSig(n) (n & (-n))
-#define mostSig(n) (1 << (fls(n) - 1)) // only for int
-#define set(n, i) (n |= (1 << i))
-#define test(n, i) (n & (1 << i))
-#define reset(n, i) (n &= ~(1 << i))
-#define flip(n, i) (n ^= (1 << i))
+int k;
+char c_temp, i_temp;
+string ans;
 
-
-int N, tint;
-char letters[1 << 10], tchar1, tchar2;
+struct tree_node {
+    tree_node* left;
+    tree_node* right;
+    char value;
+};
 
 int main() {
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N;
-    while (N--) {
-        cin >> tchar1;
+    tree_node *root = new tree_node(), *current_node;
 
+    cin >> k;
+    while (k--) {
+        cin >> c_temp;
+
+        current_node = root;
+        while (cin.get(i_temp), i_temp != '\n') {
+            if (i_temp == '0') {
+                if (current_node->left) {
+                    current_node = current_node->left;
+                } else {
+                    current_node->left = new tree_node();
+                    current_node = current_node->left;
+                }
+            } else if (i_temp == '1') {
+                if (current_node->right) {
+                    current_node = current_node->right;
+                } else {
+                    current_node->right = new tree_node();
+                    current_node = current_node->right;
+                }
+            }
+        }
+
+        current_node->value = c_temp;
     }
+
+    current_node = root;
+    while (cin >> c_temp) {
+        if (c_temp == '0') {
+            current_node = current_node->left;
+        } else if (c_temp == '1') {
+            current_node = current_node->right;
+        }
+
+        if (current_node->value) {
+            ans += current_node->value;
+            current_node = root;
+        }
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
