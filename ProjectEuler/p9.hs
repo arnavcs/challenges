ff6f1ebc13c014df0ebc144f2fa813d3fecc940d
Arnav Kumar <arnavcalgary12@gmail.com>
2021-06-18 22:42:01 -0600

    solved more problems

---
+++ ProjectEuler/p9.hs
@@ -0,0 +1,7 @@
+module P9 where
+
+prodPythagSummingTo :: Int -> Int
+prodPythagSummingTo n = let [(a, b, c)] = pythagSummingTo n in a * b * c
+
+pythagSummingTo :: Int -> [(Int, Int, Int)]
+pythagSummingTo n = [(a, b, c) | a <- [1 .. n], b <- [a .. n], c <- [n - a - b], a < b, b < c, a^2 + b^2 == c^2]
