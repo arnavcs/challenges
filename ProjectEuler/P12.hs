feec38c0bfb36c3b80d8384f95926d221bcc67a3
Arnav Kumar <arnavcalgary12@gmail.com>
2021-06-29 19:01:08 -0600

    solved P12

---
+++ ProjectEuler/P12.hs
@@ -0,0 +1,18 @@
+module P12 where
+
+import Data.List
+import P3
+
+firstTriNumOver :: Int -> Int
+firstTriNumOver n = sum [1..index]
+  where
+    index = head . dropWhile (\k -> (numDivisors . triNum $ k) <= n) $ [1..]
+
+triNum :: Int -> [(Int, Int)]
+triNum n = fmap (\x -> (x !! 0, if x !! 0 == 2 then length x - 1 else length x)) . group $ (merge (primeFactors n) (primeFactors $ n + 1))
+  where
+    merge x y = sort $ x ++ y
+
+numDivisors :: [(Int, Int)] -> Int
+numDivisors [] = 1
+numDivisors (x:xs) = (snd x + 1) * numDivisors xs
