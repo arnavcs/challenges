f366b236d03de62bc9c621b7818ecd211435a741
Arnav Kumar <arnav.kumar.sri@gmail.com>
2021-09-04 16:05:02 -0600

    solved with haskell

---
+++ CCC/Senior 2013/whoIsTaller.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student {
+private:
+  int number;
+  vector<*Student> taller, shorter;
+public:
+  bool isTaller (int other) {
+    
+};
+
+int main () {
+  int N, M, p, q;
+  cin >> N >> M;
+
+  vector<Student> allStudents;
+  allStudents.resize(N);
+
+  for (int i = 1; i <= N; i++) {
+    allStudents[i-1].number = i;
+  }
+
+  while (M--) {
+    cin >> p >> q;
+    allStudents[p-1].
+
+  return 0;
+}
---
+++ ProjectEuler/P14.hs
@@ -0,0 +1,13 @@
+module P14 where
+
+import Data.List
+import Data.Ord
+
+longestCollatzChainUnder :: Int -> Int
+longestCollatzChainUnder n = snd . (\xs -> maximumBy (comparing fst) (zip xs [1..])) . (fmap collatzChainLength) $ [1..n]
+
+collatzChainLength :: Int -> Int
+collatzChainLength 1 = 1
+collatzChainLength n
+    | even n    = 1 + collatzChainLength (n `div` 2)
+    | otherwise = 1 + collatzChainLength (3 * n + 1)
